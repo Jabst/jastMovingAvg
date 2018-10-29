d457b416d69d3fe3876e6725711d35c6bf6cb813
jast <jast@inesctec.pt>
2018-10-29 11:22:57 +0000

    fix

---
+++ movingmedian.go
@@ -0,0 +1,42 @@
+package jastMovAvg
+
+import(
+	"sort"
+)
+
+func getMedian(list []float64) (float64) {
+
+	sort.Slice(list, func(i, j float64) bool {
+		return list[i] < list[j]
+	})
+
+	if len(list) % 2 == 0 {
+		return (list[(len(list)/2)] + list[(len(list)/2) - 1]) / float64(2)
+	} else {
+		return list(len(list)/2)
+	}
+}
+
+func MovingMedian(list []float64, window int) ([]float64) {
+	ret := []float64{}
+	helper := []float64{}
+
+
+	for i, _ := range list {
+
+		for j := 0 ; j < window; j++ {
+			if i + j < len (list) {
+				helper = append(helper, list[i + j])
+			}
+		}
+
+		a := getMedian(helper)
+		ret = append(ret, a)
+
+		helper = nil
+
+	}
+
+	return ret
+
+}
